cc6f3de5c21a470a7ce727d35559506c8788e230
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-12-14 20:57:28 -0600

    changed to class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="backGround Image">
-        <img
-          src="https://images.unsplash.com/photo-1588993608283-7f0eda4438be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXVzdGlufGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-          width="100%"
-          alt="background"
-        />
-      </div>
-    </div>
-  );
-}
+import React, { Component } from "react";
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="backGround Image">
+          <img
+            src="https://images.unsplash.com/photo-1588993608283-7f0eda4438be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXVzdGlufGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            width="100%"
+            alt="background"
+          />
+          <div className="button">
+            <button>hello</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
