20c230129a7255bbd8923c8caba7ef2de9126883
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-12-15 21:16:18 -0600

    overlayed button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css";
-
 import React, { Component } from "react";
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="backGround Image">
-          <img
-            src="https://images.unsplash.com/photo-1588993608283-7f0eda4438be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXVzdGlufGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-            width="100%"
-            alt="background"
-          />
-          <div className="button">
-            <button>hello</button>
-          </div>
+      <div className="app">
+        <img
+          src="https://images.unsplash.com/photo-1588993608283-7f0eda4438be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXVzdGlufGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+          width="100%"
+          alt="pictures"
+        />
+
+        <div className="button">
+          <button>+</button>
         </div>
       </div>
     );
   }
 }
+
+export default App;
