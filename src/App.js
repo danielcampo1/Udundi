e1fef7a1603c3e7bbc66b3b6af058947b30bc1c1
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-12-17 21:30:59 -0600

    button and explore are in general area

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ class App extends Component {
       <div className="app">
         <img
           src="https://images.unsplash.com/photo-1588993608283-7f0eda4438be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXVzdGlufGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-          width="100%"
           alt="pictures"
         />
 
+        <div className="text">
+          <h1> Explore </h1>
+        </div>
         <div className="button">
           <button>+</button>
         </div>
